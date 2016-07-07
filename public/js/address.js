5b5768e714c94fcf4715ab9569ad49ba4d184330
theresa le <trle@wolfmail.stritch.edu>
2016-07-07 18:35:49 -0500

    Input from user can get weather of address they put in!

--- public/js/address.js
+++ public/js/address.js
@@ -2,10 +2,27 @@
 
 function Address(){
 
+	/*this.getLocationWithInput = function(callback){
+		var url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+		$.getJSON(url, function(data){
+			callback(place.geometry.viewport.f.b,place.geometry.viewport.b.b)
+			//callback(latitude,longitude)
+		});
+	}*/
+
+	this.getWeather = function(latitude,longitude){
+		$.getJSON ("/weather?latitude="+latitude+"&longitude="+longitude,function(data){
+			console.log(data.daily);
+		});
+	}
+}
+	
+
+	/*
+	finds lat & lng from address
 	this.getLocationWithInput = function(){
 		var url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDp2dB8A9x9BAGplFjWAqWHNUi256c6pWk";
 		$.getJSON(url, function(data){
 			console.log(data)
 		});
-	}
-}
+	}*/
